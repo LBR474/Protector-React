cf3203cb2edd982a6c4e4f0c78f242ba55fa6646
LBR474 <LB572179@gmail.com>
2022-09-29 18:32:19 +1000

    Add full name to control app

--- src/ControlApp.js
+++ src/ControlApp.js
@@ -20,9 +20,9 @@ export function ControlApp() {
   return (
     <>
       <Routes>
-        <Route path="/GQGame" element={<GQGame />} />
+        <Route path="/react-three/GQGame" element={<GQGame />} />
         <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/react-three" element={<Home />} />
         </Route>
       </Routes>
     </>
